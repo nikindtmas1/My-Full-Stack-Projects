d6cd9a3c471af6ed9ab41742c2624d1104838b01
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-05-13 20:13:56 +0300

    Added code client / server

---
+++ Full-Stack-Project-Memo/Client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid  } from '@material-ui/core';
+
+const App = () => {
+    return (
+    <div>
+        <h1>App Project</h1>
+    </div>
+    )
+}
+
+export default App;
