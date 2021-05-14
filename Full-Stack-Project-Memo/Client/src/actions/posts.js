816b6a1288a272351d87fcffd34347c61c4106f6
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-05-14 22:18:11 +0300

    Added reducers and actions logic

---
+++ Full-Stack-Project-Memo/Client/src/actions/posts.js
@@ -0,0 +1,15 @@
+import * as api from '../api';
+
+const getPosts = () => async (dispatch) => {
+
+    try {
+        
+    } catch (error) {
+        
+    }
+
+
+    const action = { type: 'FETCH_ALL', payload: [] }
+
+    dispatch(action);
+}
