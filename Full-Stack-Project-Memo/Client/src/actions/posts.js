45ced79fc4dd18336d30414566c949d9325df27d
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-05-16 22:21:54 +0300

    Added code reducer

--- Full-Stack-Project-Memo/Client/src/actions/posts.js
+++ Full-Stack-Project-Memo/Client/src/actions/posts.js
@@ -1,15 +1,12 @@
 import * as api from '../api';
 
-const getPosts = () => async (dispatch) => {
+export const getPosts = () => async (dispatch) => {
 
     try {
-        
+        const { data } = await api.fetchPosts();
+        dispatch({ type: "FETCH_ALL", payload: data });
     } catch (error) {
-        
+        console.log(error.message)
     }
 
-
-    const action = { type: 'FETCH_ALL', payload: [] }
-
-    dispatch(action);
 }
