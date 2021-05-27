c9119cdb47eb0ad4e228891d2866a3465020786f
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-05-27 22:31:56 +0300

    Added actions/posts.js

--- Full-Stack-Project-Memo/Client/src/actions/posts.js
+++ Full-Stack-Project-Memo/Client/src/actions/posts.js
@@ -1,10 +1,12 @@
+import { FETCH_ALL, UPDATE, CREATE, DELETE } from '../constants/actionTypes';
+
 import * as api from '../api';
 
 export const getPosts = () => async (dispatch) => {
 
     try {
         const { data } = await api.fetchPosts();
-        dispatch({ type: "FETCH_ALL", payload: data });
+        dispatch({ type: FETCH_ALL, payload: data });
     } catch (error) {
         console.log(error.message)
     }
@@ -15,7 +17,7 @@ export const createPost = (post) => async (dispatch) => {
     try {
         const { data } = await api.createPost(post);
 
-        dispatch({ type: "CREATE", payload: data });
+        dispatch({ type: CREATE, payload: data });
     } catch (error) {
         console.log(error);
     }
@@ -25,7 +27,7 @@ export const updatePost = (id, post) => async (dispatch) => {
     try {
       const { data } = await api.updatePost(id, post);
 
-      dispatch({ type: 'UPDATE', payload: data });
+      dispatch({ type: UPDATE, payload: data });
     } catch (error) {
         console.log(error);
     }
@@ -35,7 +37,7 @@ export const deletePost = (id) => async (dispatch) => {
     try {
         await api.deletePost(id);
 
-        dispatch({ type: 'DELETE', payload: id });
+        dispatch({ type: DELETE, payload: id });
     } catch (error) {
         console.log(error);
     }
@@ -45,7 +47,7 @@ export const likePost = (id) => async (dispatch) => {
     try {
         const { data } = await api.likePost(id);
 
-        dispatch({ type: 'UPDATE', payload: data });
+        dispatch({ type: UPDATE, payload: data });
     } catch (error) {
         console.log(error);
     }
